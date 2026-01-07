2ba68a41dd01a6627d955c8bb87ad7a8206f8640
Mohan <mohanroda12@gmail.com>
2026-01-07 00:10:31 +0000

    Cleaned up simulation class to make it an actual class (not all in main method)

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,27 +1,57 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Simulation {
-    public static void main(String[] args) {
-        AstronomicalObject earth = new Planet("Earth", Planet.EARTH_RADIUS, Planet.EARTH_MASS);
-        Particle ball = new Particle(0.5, 0.5, 5, 0.05, 0.5);
 
-        ParticleBoxPanel sim = new ParticleBoxPanel(ball);
-        ParticleBoxFrame frame = new ParticleBoxFrame(sim);
+    private final double SPEED = 1; // How fast the program runs
+    private final double DT = 0.001; // Time between physics calculations
 
-        final double SPEED = 1; // How fast the program runs
-        final double DT = 0.001; // Time between calculations
+    ArrayList<Particle> particles; //List of all particles to be shown in simulation
+    Planet planet; //Planet that the particles will simulate on
 
+    public Simulation(Planet planet, ArrayList<Particle> particles) {
+        this.planet = planet;
+        this.particles = particles;
+    }
+
+    public void simulateTick(ParticleBoxPanel simUI) {
+        for (Particle particle : particles) {
+            double forceY = particle.forceDueToGravity(planet);
+            double forceX = particle.frictionalForce(particle.getDampingFactor(), planet);
+            particle.updateParticle(forceX, forceY, DT * SPEED);
+            simUI.repaint();
+        }
+    }
+
+    public void simulate(ParticleBoxPanel simUI) {
         Timer timer = new Timer((int)(1 / SPEED), new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double forceY = ball.forceDueToGravity(earth);
-                ball.updateParticle(0, forceY, DT * SPEED);
-                //System.out.println(ball.getYPosition());
-                sim.repaint();
+                simulateTick(simUI);
             }
         });
         timer.start();
+    }
+
+    public static void main(String[] args) {
+        // Particles to simulate
+        ArrayList<Particle> particles = new ArrayList<>();
+        Particle ball = new Particle(0.5, 0.5, 5, 0.05, 0.45);
+        particles.add(ball);
+        Particle ball2 = new Particle(0.3, 0.6, 2, 0.08, 0.65);
+        particles.add(ball2);
+        Particle ball3 = new Particle(0.5, 0.5, 20, 0.02, 0.75);
+        particles.add(ball3);
+
+
+        Planet earth = new Planet("Earth", Planet.EARTH_RADIUS, Planet.EARTH_MASS);
+
+        ParticleBoxPanel simUI = new ParticleBoxPanel(particles);
+        ParticleBoxFrame frame = new ParticleBoxFrame(simUI);
+
+        Simulation simulation = new Simulation(earth, particles);
+        simulation.simulate(simUI);
     }
 }
