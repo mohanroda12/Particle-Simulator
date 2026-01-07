6647c5a676d97e7b9a8349e67941b29fdd2d29ba
Mohan <mohanroda12@gmail.com>
2026-01-07 00:11:11 +0000

    Allow for multiple particles to be simulated in GUI

--- src/ParticleBoxPanel.java
+++ src/ParticleBoxPanel.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class ParticleBoxPanel extends JPanel {
-    private Particle particle;
+    private ArrayList<Particle> particles;
 
-    public ParticleBoxPanel(Particle particle) {
-        this.particle = particle;
+    public ParticleBoxPanel(ArrayList<Particle> particles) {
+        this.particles = particles;
         this.setPreferredSize(new Dimension(750, 750));
     }
 
@@ -19,25 +20,27 @@ public class ParticleBoxPanel extends JPanel {
 
         int scaleFactor = getWidth(); // Width = 1x1 m
 
-        // Draw particle
-        g2.setColor(Color.GREEN);
-        double radius = particle.getRadius();
+        // Draw particles
+        for (Particle particle : particles) {
+            g2.setColor(Color.GREEN);
+            double radius = particle.getRadius();
 
-        g2.fillOval((int)Math.round((particle.getXPosition() - radius) * scaleFactor),
-                (int)Math.round(getHeight() - ((particle.getYPosition() + radius) * scaleFactor)),
-                (int)Math.round(radius * scaleFactor * 2),
-                (int)Math.round(radius * scaleFactor * 2));
+            g2.fillOval((int) Math.round((particle.getXPosition() - radius) * scaleFactor),
+                    (int) Math.round(getHeight() - ((particle.getYPosition() + radius) * scaleFactor)),
+                    (int) Math.round(radius * scaleFactor * 2),
+                    (int) Math.round(radius * scaleFactor * 2));
 
-        g2.setColor(Color.BLACK);
-        g2.setFont(new Font(Font.DIALOG_INPUT, Font.BOLD, 20));
-        String xPos = String.format("%.2f", particle.getXPosition());
-        String yPos = String.format("%.2f", particle.getYPosition());
-        String xVelocity = String.format("%.2f", particle.getVelocityX());
-        String yVelocity = String.format("%.2f", particle.getVelocityY());
-        g2.drawString("X: " + xPos, 50, 50);
-        g2.drawString("Y: " +yPos, 50, 80);
+            g2.setColor(Color.BLACK);
+            g2.setFont(new Font(Font.DIALOG_INPUT, Font.BOLD, 20));
+            String xPos = String.format("%.2f", particles.getFirst().getXPosition());
+            String yPos = String.format("%.2f", particles.getFirst().getYPosition());
+            String xVelocity = String.format("%.2f", particles.getFirst().getVelocityX());
+            String yVelocity = String.format("%.2f", particles.getFirst().getVelocityY());
+            g2.drawString("X: " + xPos, 50, 50);
+            g2.drawString("Y: " + yPos, 50, 80);
 
-        g2.drawString("X velocity: " + xVelocity, 500, 50);
-        g2.drawString("Y velocity: " +yVelocity, 500, 80);
+            g2.drawString("X velocity: " + xVelocity, 500, 50);
+            g2.drawString("Y velocity: " + yVelocity, 500, 80);
+        }
     }
 }
